07f41edbf944f7c24217d5672b29b98e51b646f9
flyfly911 <flyfly911@hotmail.com>
2020-03-03 22:15:59 -0500

    lenovo

--- src/main/java/com/robin/sbdemo/Controller/SbdemoController.java
+++ src/main/java/com/robin/sbdemo/Controller/SbdemoController.java
@@ -1,7 +1,6 @@
 package com.robin.sbdemo.Controller;
 
 
-
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,59 +12,68 @@ import java.util.ArrayList;
 @SpringBootApplication
 public class SbdemoController {
     @RequestMapping("/")
-    public String spdemo(){
+    public String spdemo() {
         return calc();
 
     }
-private String checkandreturn(int i){
-    int[] ilist={9,8,7,6,5,4,3,2,1};
-        ArrayList <Integer> ical=new ArrayList<Integer>();
-        int x=i;
-        while(x>=3){
-            int div=0;
-            int mod=0;
-            div=x%3;
-            mod=x/3;
-            if(mod>3){
+
+    private String checkandreturn(int i) {
+        int[] ilist = {9, 8, 7, 6, 5, 4, 3, 2, 1};
+        ArrayList<Integer> ical = new ArrayList<Integer>();
+        int x = i;
+        if(x<3){
+            ical.add(x);
+        }
+        while (x >= 3) {
+            int div = 0;
+            int mod = 0;
+            div = x % 3; //取余
+            mod = x / 3; //取整
+            if (mod > 3) {
                 ical.add(div);
             } else {
                 ical.add(mod);
             }
-            x=x/3;
+            x = x / 3;
+            if(x<3){
+                ical.add(x);
+            }
         }
-        int ilen=ical.size();
+        int ilen = ical.size();
         int zeronum;
-        for(zeronum=0;zeronum<8-ilen;zeronum++){
+        for (zeronum = 0; zeronum < 8 - ilen; zeronum++) {
             ical.add(0);
         }
+        System.out.println(ical.toString());
         //用三进制0，1，2表示连接，加和减
-    int itmp1=0;
-        int itmp2=0;
-        for(int ipos=0;ipos<8;ipos++){
-          int icalc=  ical.get(7-ipos);
-          switch (icalc){
-              case 0: //连接
-                      itmp1=ilist[ipos]*10+ilist[ipos+1];
-                      ipos++;
-                      break;
+        int itmpleft = 0;
+        int itmpright = 0;
+        for (int ipos = 0; ipos < 8; ipos++) {
+            //取操作符
+            int icalc = ical.get(7 - ipos);
+            switch (icalc) {
+                case 0: //连接
+                    itmpleft = ilist[ipos] * 10 + ilist[ipos + 1];
+                    ipos++;
+                    break;
 
-              case 1: //做加法
+                case 1: //做加法
 
-              case 2: //做减法
-          }
+                case 2: //做减法
+            }
         }
 
 
         return "";
-}
+    }
 
-    private String calc(){
+    private String calc() {
 
-        ArrayList <String> rStr = new ArrayList();
+        ArrayList<String> rStr = new ArrayList();
 
-        for(int i=0;i<Math.pow(3,8);i++){
-            String str=checkandreturn(i);
-            if(str!=""){
+        for (int i = 0; i < Math.pow(3, 8); i++) {
+            String str = checkandreturn(i);
+            if (str != "") {
                 rStr.add(str);
             }
         }
