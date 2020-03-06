2d769224883860176db7d883068370dc8e231c14
RobinBrossard <1711666@qq.com>
2020-03-05 22:16:06 -0500

    hp finish

--- src/main/java/com/robin/sbdemo/Controller/SbdemoController.java
+++ src/main/java/com/robin/sbdemo/Controller/SbdemoController.java
@@ -2,10 +2,19 @@ package com.robin.sbdemo.Controller;
 
 
 import com.robin.sbdemo.lib.CommonLib;
+import com.robin.sbdemo.lib.ReadExcelTools;
+import org.apache.poi.openxml4j.opc.internal.ContentType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import sun.security.action.OpenFileInputStreamAction;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 
@@ -15,9 +24,19 @@ public class SbdemoController {
     @RequestMapping("/")
     public String spdemo() {
         return CommonLib.calc();
-
     }
 
+    @RequestMapping("/a")
+    public String readxls() throws IOException {
 
+        File file=new File("d:/abc.xlsx");
+        MultipartFile multipartFile=new MockMultipartFile(
+                "d:/abc.xlsx",
+                new FileInputStream(file)
+        );
+
+
+        return ReadExcelTools.readExcel(multipartFile).toString();
+    }
 
 }
